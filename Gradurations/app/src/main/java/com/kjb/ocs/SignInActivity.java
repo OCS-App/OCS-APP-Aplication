b024a1500bf76f702758f1c2cda6b050b18afd27
Ganghun <41173859+Ganghun@users.noreply.github.com>
2020-05-26 18:27:23 +0900

    Register_Test01

--- Gradurations/app/src/main/java/com/kjb/ocs/SignInActivity.java
+++ Gradurations/app/src/main/java/com/kjb/ocs/SignInActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.MultiAutoCompleteTextView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -18,17 +19,12 @@ public class SignInActivity extends AppCompatActivity {
         Spinner classSpinner = findViewById(R.id.id_class);
         Spinner classnumSpinner = findViewById(R.id.id_classnum);
         Spinner numSpinner = findViewById(R.id.id_num);
+        Spinner majorSpinner = findViewById(R.id.major_array);
 
-        final ArrayAdapter classAdapter = ArrayAdapter.createFromResource(this, R.array.id_class,android.R.layout.simple_spinner_dropdown_item);
+        final ArrayAdapter classAdapter = ArrayAdapter.createFromResource(this,
+                R.array.id_class,android.R.layout.simple_spinner_item);
+        classAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         classSpinner.setAdapter(classAdapter);
-        classSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(SignInActivity.this,
-                        (Integer) classAdapter.getItem(i),Toast.LENGTH_SHORT).show();
-            }
-
-        });
 
         ArrayAdapter classnumAdapter = ArrayAdapter.createFromResource(this,
                 R.array.id_classnum, android.R.layout.simple_spinner_item);
@@ -39,6 +35,13 @@ public class SignInActivity extends AppCompatActivity {
                 R.array.id_num, android.R.layout.simple_spinner_item);
         numAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         numSpinner.setAdapter(numAdapter);
+
+        ArrayAdapter majorAdapter = ArrayAdapter.createFromResource(this,
+                R.array.major,android.R.layout.simple_spinner_item);
+        majorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        majorSpinner.setAdapter(majorAdapter);
+
+
 
     }
 
